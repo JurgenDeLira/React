66c3f6ad4fd070b932395a1bb1e37772bf117f93
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-10-06 10:54:03 -0700

    Importar exportar módulos JS

---
+++ 1-intro-js/src/12-arreglos-funciones.js
@@ -0,0 +1,105 @@
+const paper = {
+    product: 'paper',
+    price: 100,
+    quantity: 10,
+}
+
+const invoices = [
+    {
+    id: 1,
+    name: 'Compras de oficina',
+    client: {
+
+        name: 'Maria',
+        lastName: 'Doe',
+
+        },
+        items: [//Esto es un array
+            {//Esto es un objeto
+                product: 'keyboard',
+                price: 399,
+                quantity: 2,
+            },
+            {
+                product: 'mouse',
+                price: 200,
+                quantity: 1
+            },
+            paper,
+        ]
+    },
+        {
+        id: 2,
+        name: 'Compras de computación',
+     
+        client: {
+ 
+            name: 'Pepe',
+            lastName: 'Doe',
+ 
+        },
+        items: [//Esto es un array
+            {//Esto es un objeto
+                product: 'keyboard',
+                price: 399,
+                quantity: 2,
+            },
+            {
+                product: 'screen 17',
+                price: 800,
+                quantity: 1
+            },
+            {
+                product: 'cpu intel',
+                price: 1000,
+                quantity: 10
+            },
+        ]
+    },
+        {
+        id: 3,
+        name: 'Compras de papeleria',
+
+        client: {
+   
+            name: 'Jhon',
+            lastName: 'Doe',
+      
+        },
+        items: [//Esto es un array
+            {
+                product: 'pencil',
+                price: 50,
+                quantity: 1
+            },
+            paper,
+        ]
+    }
+];
+
+const invoicesName = invoices.map( i => i.name);
+console.log(invoices)
+console.log(invoicesName)
+
+const invoicesClient = invoices.map( i => i.client.name);
+
+console.log(invoicesClient)
+
+const invoiceById = invoices.find( i=> i.id === 2)
+console.log(invoiceById)
+
+const invoiceByClientName = invoices.find( i=> i.name === 'Pepe')
+console.log(invoiceByClientName)
+
+const invoiceFilter = invoices.filter( i => i.id > 1)
+console.log(invoiceByClientName)
+
+console.log('filter eliminar')
+const invoiceDeleted = invoices.filter( i => i.id != 2)
+console.log(invoiceDeleted)
+
+const invoiceFilter2 = invoices.filter(i => i.items.includes(paper) )
+console.log(invoiceFilter2)
+
+const result = invoices.some(i => i.client.name === 'Pepe');
+console.log(result)
---
+++ 1-intro-js/src/data/invoices.js
@@ -0,0 +1,82 @@
+export const paper = {
+    product: 'paper',
+    price: 100,
+    quantity: 10,
+}
+
+export const invoices = [
+    {
+    id: 1,
+    name: 'Compras de oficina',
+    client: {
+
+        name: 'Maria',
+        lastName: 'Doe',
+
+        },
+        items: [//Esto es un array
+            {//Esto es un objeto
+                product: 'keyboard',
+                price: 399,
+                quantity: 2,
+            },
+            {
+                product: 'mouse',
+                price: 200,
+                quantity: 1
+            },
+            paper,
+        ]
+    },
+        {
+        id: 2,
+        name: 'Compras de computación',
+     
+        client: {
+ 
+            name: 'Pepe',
+            lastName: 'Doe',
+ 
+        },
+        items: [//Esto es un array
+            {//Esto es un objeto
+                product: 'keyboard',
+                price: 399,
+                quantity: 2,
+            },
+            {
+                product: 'screen 17',
+                price: 800,
+                quantity: 1
+            },
+            {
+                product: 'cpu intel',
+                price: 1000,
+                quantity: 10
+            },
+        ]
+    },
+        {
+        id: 3,
+        name: 'Compras de papeleria',
+
+        client: {
+   
+            name: 'Jhon',
+            lastName: 'Doe',
+      
+        },
+        items: [//Esto es un array
+            {
+                product: 'pencil',
+                price: 50,
+                quantity: 1
+            },
+            paper,
+        ]
+    }
+];
+
+ export const invoiceByClientName = (clientName) => {
+    return invoices.find( i=> i.client.name === clientName);
+}
