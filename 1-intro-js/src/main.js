68af8a17f2a10737a5aaa393772e6664a8271b68
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-10-06 13:00:56 -0700

    API DOM Document Object Model

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -7,10 +7,21 @@
 
 const findAllUsers = async() => {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await response.json();
-    return data;
+    const users = await response.json();
+    const ul = document.createElement('ul');
 
+    users.forEach(user => {
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        ul.append(li);
+        console.log(user.name);
+    })
+
+    document.getElementById('root').append(ul);
 }
-const users = await findAllUsers();
-console.log(users)
+
+findAllUsers();
+
+//const users = await findAllUsers();
+//console.log(users)
 console.log('Hola que tal');
