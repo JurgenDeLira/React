44d65ea1e74f9dfaee7885a7a2b54ea7497fba27
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-10-06 12:53:30 -0700

    async/await

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -1,14 +1,16 @@
 
-const httpClient =  fetch('https://jsonplaceholder.typicode.com/users');
 
-/*httpClient.then( response => {
-    response.json().then(data => {
-        console.log(data)
-    });
-});*/
 
-httpClient
+/*httpClient
     .then( response => response.json())
-    .then(data => console.log(data))
+    .then(data => console.log(data));*/
 
-console.log('Hola que tal')
+const findAllUsers = async() => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    return data;
+
+}
+const users = await findAllUsers();
+console.log(users)
+console.log('Hola que tal');
