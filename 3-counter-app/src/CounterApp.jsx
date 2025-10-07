4e7646634c479acadab4a07ee411ac9681735737
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-10-07 13:34:44 -0700

    componente counter

--- 3-counter-app/src/CounterApp.jsx
+++ 3-counter-app/src/CounterApp.jsx
@@ -1,7 +1,16 @@
 
 export const CounterApp = () => {
 
+    let counter = 0;
+    const counterIncrement = () => {
+        counter = counter + 1;
+        console.log('click!' + counter)
+    }
+
     return <>
-    
+    <h2>El valor del contador es {counter }</h2>
+    <button onClick={() => {
+        counterIncrement()
+    }} >incrementar contador +1</button>
     </>
-}
+}  
