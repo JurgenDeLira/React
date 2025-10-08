455bc0b1ff8b9b5ad9c3fba41a096056fe18f73e
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-10-08 12:16:58 -0700

    componente en subcomponentes

--- 4-invoice-app/src/main.jsx
+++ 4-invoice-app/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { InvoiceApp } from './components/Invoice.App'
+import { InvoiceApp } from './components/InvoiceApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
