38de483172b3716f0f0885da997c4fe31336edee
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-10-07 17:39:31 -0700

    function service

--- 4-invoice-app/src/components/Invoice.App.jsx
+++ 4-invoice-app/src/components/Invoice.App.jsx
@@ -1,8 +1,15 @@
-export const InvoiceApp = () => {
+import { getInvoice } from "../services/getInvoice"
 
+export const InvoiceApp = () => {
+    
+    const invoice = getInvoice();
     return (
         <>
             <h1>Ejemplo Factura</h1>
+            <ul>
+                <li>Id: { invoice.id }</li>
+                <li>name: { invoice.name }</li>
+            </ul>
         </>
     )
 }
