2dcf239b603d00bd5993e2a4f6ac9cca4e1de4bf
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-10-09 15:49:20 -0700

    Form un solo state para los inputs

--- 4-invoice-app/src/InvoiceApp.jsx
+++ 4-invoice-app/src/InvoiceApp.jsx
@@ -8,58 +8,62 @@ import { TotalView } from "./components/TotalView";
 
 export const InvoiceApp = () => {
 
-    const {total, id, name, client, company, items: itemsInitial } = getInvoice();
+    const { total, id, name, client, company, items: itemsInitial } = getInvoice();
 
-    const [productValue, setProductValue] = useState ('');
-    const [priceValue, setPriceValue] = useState ('');
-    const [quantityValue, setQuantityValue] = useState ('');
+    const [formItemsState, setFormItemsState] = useState({
+        product: '',
+        price: '',
+        quantity: '',
+    });
+
+    const { product, price, quantity } = formItemsState;
 
     const [items, setItems] = useState(itemsInitial);
 
     const [counter, setCounter] = useState(4);
 
-    const onProductChange = ({target}) => {
-            console.log(target.value);
-            setProductValue(target.value);
-        }
-    
-    const onPriceChange = ({target}) => {
-                                console.log(target.value);
-                                setPriceValue(target.value);
-                            }
-    
-    const onQuantityChange = ({target}) => {
-                                console.log(target.value);
-                                setQuantityValue(target.value);
-                            }
-    
-    const onInvoiceItemsSubmit = (event) => {
-                            event.preventDefault();
+    const onInputChange = ({ target: {name, value} }) => {
+        console.log(name);
+        console.log(value);
 
-                            if(productValue.trim().length <= 1) return;
-                            if(priceValue.trim().length <= 1) return;
-                            if(isNaN(priceValue.trim())) {
-                                alert('Error el precio no es un número')
-                                return;
-                            }
-                            if(quantityValue.trim().length < 1) {
-                                alert('Error la cantidad tiene que ser mayor a 0')
-                                return;
-                            }
-                            if(isNaN(quantityValue.trim())) {
-                                alert('Error la cantidad no es un número')
-                                return;
-                            }
-                            setItems([...items, {
-                                id: counter, 
-                                product: productValue.trim(), 
-                                price: +priceValue.trim(), 
-                                quantity: parseInt(quantityValue.trim(), 10)}]);
-                                setProductValue('');
-                                setPriceValue('');
-                                setQuantityValue('');
-                                setCounter(counter + 1);
-                        }
+        setFormItemsState({
+            ...formItemsState,
+            [ name ]: value
+        });
+    }
+
+    
+
+    const onInvoiceItemsSubmit = (event) => {
+        event.preventDefault();
+
+        if (product.trim().length <= 1) return;
+        if (price.trim().length <= 1) return;
+        if (isNaN(price.trim())) {
+            alert('Error el precio no es un número')
+            return;
+        }
+        if (quantity.trim().length < 1) {
+            alert('Error la cantidad tiene que ser mayor a 0')
+            return;
+        }
+        if (isNaN(quantity.trim())) {
+            alert('Error la cantidad no es un número')
+            return;
+        }
+        setItems([...items, {
+            id: counter,
+            product: product.trim(),
+            price: +price.trim(),
+            quantity: parseInt(quantity.trim(), 10)
+        }]);
+        setFormItemsState({
+        product: '',
+        price: '',
+        quantity: '',
+    });
+        setCounter(counter + 1);
+    }
 
 
     return (
@@ -74,50 +78,50 @@ export const InvoiceApp = () => {
                     </div>
 
                     <div className="card-body">
-                        <InvoiceView id={ id } name={name}/>
+                        <InvoiceView id={id} name={name} />
 
                         <div className="row my-3">
 
                             <div className="col">
-                                <ClientView title="Datos del cliente" client={client}/>
-                                
+                                <ClientView title="Datos del cliente" client={client} />
+
                             </div>
 
                             <div className="col">
-                                <CompanyView title="Datos de la empresa" company={company}/>
+                                <CompanyView title="Datos de la empresa" company={company} />
                             </div>
                         </div>
 
                         <ListItemsView title="Productos de la factura" items={items} />
-                        <TotalView total={ total } />
+                        <TotalView total={total} />
                         <form className="w-50" onSubmit={onInvoiceItemsSubmit}>
-                            <input 
-                            type="text" 
-                            name="product" 
-                            value={ productValue }
-                            placeholder="Producto" 
-                            className="form-control m-3" 
-                            onChange={onProductChange}/>
-                            <input 
-                            type="text" 
-                            name="price"
-                            value={ priceValue }
-                            placeholder="Precio" 
-                            className="form-control m-3" 
-                            onChange={event => onPriceChange(event)}/>
-                            <input 
-                            type="text" 
-                            name="quantity" 
-                            value={ quantityValue }
-                            placeholder="Cantidad" 
-                            className="form-control m-3" 
-                            onChange={onQuantityChange}/>
-                            
-                            <button 
-                            type="submit" 
-                            className="btn btn-primary m-3">
+                            <input
+                                type="text"
+                                name="product"
+                                value={product}
+                                placeholder="Producto"
+                                className="form-control m-3"
+                                onChange={onInputChange} />
+                            <input
+                                type="text"
+                                name="price"
+                                value={price}
+                                placeholder="Precio"
+                                className="form-control m-3"
+                                onChange={event => onInputChange(event)} />
+                            <input
+                                type="text"
+                                name="quantity"
+                                value={quantity}
+                                placeholder="Cantidad"
+                                className="form-control m-3"
+                                onChange={onInputChange} />
+
+                            <button
+                                type="submit"
+                                className="btn btn-primary m-3">
                                 Nuevo Item
-                                </button>
+                            </button>
                         </form>
                     </div>
                 </div>
